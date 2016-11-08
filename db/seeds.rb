44c80e1119ac822d12f5db1f40efaf888c9cf87e
noodi36 <noodi36@naver.com>
2016-11-08 06:11:13 +0000

    ajax_form_tag

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Book.create(title: "안드로이드 개발자를 위한 자바", price: "33600", publish: "제이펍")
+Book.create(title: "자바스크립트 라이브러리 실전 활용", price: "27090", publish: "제이펍")
+Book.create(title: "Ruby on Rails 4 포켓 레퍼런스", price: "29190", publish: "인사이트")
+Book.create(title: "처음 시작하는 안드로이드 애플리케이션 개발", price: "31500", publish: "한빛아카데미")
+Book.create(title: "혼자 공부하는 java서버 사이드 개발", price: "37800", publish: "한빛미디어")
+Book.create(title: "애플리케이션 개발 입문", price: "19950", publish: "인사이트")
